7ba4b6488f6c839c5092d24e1195de1cd325fd39
samguns <wang.vai@gmail.com>
2019-02-24 18:47:35 +0800

    Add markers

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-git clone https://github.com/ros-perception/slam_gmapping.git
+// git clone https://github.com/ros-perception/slam_gmapping.git
 
-git clone https://github.com/turtlebot/turtlebot.git
+// git clone https://github.com/turtlebot/turtlebot.git
 
-git clone https://github.com/turtlebot/turtlebot_interactions.git
+// git clone https://github.com/turtlebot/turtlebot_interactions.git
 
-git clone https://github.com/turtlebot/turtlebot_simulator.git
+// git clone https://github.com/turtlebot/turtlebot_simulator.git
 
-rosdep install --from-paths src --ignore-src -r -y
+rosdep install --from-paths catkin_ws/src --ignore-src -r -y
---
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = 1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+  // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending pickup goal");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Hooray, we moved to pickup goal and waiting 5 seconds");
+    ros::Duration(5.0).sleep();
+  }
+  else
+    ROS_INFO("The base failed to move to pickup goal for some reason");
+
+
+  goal.target_pose.pose.position.x = -1.0;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+  // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending drop off goal");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, we moved to drop off goal");
+  else
+    ROS_INFO("The base failed to move to drop off goal for some reason");
+
+  return 0;
+}
